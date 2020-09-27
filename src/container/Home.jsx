b48da862422805d2842ec5e3f119213da18b5bf7
Guilherme Milfont <guilherme.milfont@gmail.com>
2020-09-27 02:22:10 -0300

    feat(marvel): created order logic and start to remove repeated items on fav arr

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -1,21 +1,58 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Home = ({ characters }) => {
+  const [data, setData] = useState([]);
+  const [sort, setSort] = useState(false);
+  const [favArr, setFavArr] = useState([]);
+  const [currentFavID, setCurrentFavID] = useState();
+
+  useEffect(() => {
+    setData(characters);
+  }, [characters]);
+
+  useEffect(() => {
+    const orded = (type) => {
+      if (type) {
+        return [...data].sort((a, b) => b.name.localeCompare(a.name));
+      }
+      return [...data].sort((a, b) => a.name.localeCompare(b.name));
+    };
+    setData(orded(sort));
+  }, [sort]);
+
   if (characters === undefined) return <h1>Loading...</h1>;
 
-  const renderList = () => {
-    const char = characters.map((character) => <li><img src={character.thumbnail.path} /></li>);
-
-    return char;
+  const handleOrderByName = () => {
+    setSort(!sort);
   };
+
+  const handleFavorite = (id) => {
+    setCurrentFavID(id);
+    if (favArr.length < 5) {
+      if (currentFavID !== id) {
+        setFavArr(favArr.concat([{ id }]));
+      }
+    }
+  };
+
+  console.log({ currentFavID });
+
+  console.log('depois', favArr);
 
   return (
     <>
       <h1>Home</h1>
+      <span onClick={() => handleOrderByName()}>Ordernar por nome A/Z</span>
+      <br />
+      <span>Listar por favoritos</span>
       <ul>
-        {renderList()}
+        {data.map((character) => (
+          <li>
+            {character.name}
+            <span onClick={() => handleFavorite(character.id)}> favoritar</span>
+          </li>
+        ))}
       </ul>
-
     </>
   );
 };
--- src/utils/fetchCharacters.js
+++ src/utils/fetchCharacters.js
@@ -1,14 +1,12 @@
-const fetchCharacters = fetch('https://gateway.marvel.com:443/v1/public/characters?apikey=d54085a32009432805ffa8746025c8f5&hash=056c2dbfa6bc4260c65e8235dba5dc64&ts=1600986257891')
-  .then(
-    (res) => {
-      if (res.ok) {
-        return res.json();
-      }
-      throw Error('API doesn`t respond');
-    },
-  )
-  .then(
-    (result) => result.data.results,
-  );
+const fetchCharacters = fetch(
+  'https://gateway.marvel.com:443/v1/public/characters?apikey=d54085a32009432805ffa8746025c8f5&hash=056c2dbfa6bc4260c65e8235dba5dc64&ts=1600986257891',
+)
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+    throw Error('API doesn`t respond');
+  })
+  .then((result) => result.data.results);
 
 export default fetchCharacters;
